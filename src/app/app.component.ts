c7ab99f96fb5954fb411250b669552ea68611bbd
willingmaster <i.webdev2018@gmail.com>
2020-11-26 22:17:47 -0500

    kendo ui.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'angular112720';
+  onButtonClick() {
+      this.title = 'Hello from Kendo UI!';
+  }
+  public kendokaAvatar = 'https://www.telerik.com/kendo-angular-ui-develop/components/navigation/appbar/assets/kendoka-angular.png';
+	public opened = false;
+
+	public close(status: any) {
+	  console.log(`Dialog result: ${status}`);
+	  this.opened = false;
+	}
+
+	public open() {
+	  this.opened = true;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    IndicatorsModule,
+    IconsModule,
+    InputsModule,
+    NavigationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
